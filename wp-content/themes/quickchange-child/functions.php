910155fdb233bac1931de6cdd5678613079369ff
Adam Lopez <adlopez@masonite.com>
2020-12-21 13:26:09 +0000

    Adding a commit to the functions file for testing

--- wp-content/themes/quickchange-child/functions.php
+++ wp-content/themes/quickchange-child/functions.php
@@ -4,3 +4,5 @@ function my_custom_scripts() {
     wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/assets/script.js', array( 'jquery' ),'',true );
 }
 add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
+
+// this is a commit 
