178ba2aa7d616483e32748a81085646c73ce801b
Adam Lopez <adlopez@masonite.com>
2020-12-21 13:14:36 +0000

    All uploaded images and icons for Quick CHange. All of the uploaded elementor styles are also within this commit. Changes made to the child functions theme have been made as well

--- wp-content/themes/quickchange-child/functions.php
+++ wp-content/themes/quickchange-child/functions.php
@@ -1,2 +1,6 @@
 <?php
 /* Write your awesome functions below */
+function my_custom_scripts() {
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/assets/script.js', array( 'jquery' ),'',true );
+}
+add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
