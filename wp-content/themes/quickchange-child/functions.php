ab4913467b9b620073a8a9db4fa33040ebb04464
Adam Lopez <adlopez@masonite.com>
2020-12-21 14:08:08 +0000

    Third test to pushing code to the server for quick change

--- wp-content/themes/quickchange-child/functions.php
+++ wp-content/themes/quickchange-child/functions.php
@@ -6,4 +6,5 @@ function my_custom_scripts() {
 add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
 
 // this is a commit
-// This is another commit 
+// This is another commit
+// Third time is a charm
